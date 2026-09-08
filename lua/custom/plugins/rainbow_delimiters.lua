0a858d9c1a52aec30ba1985ae58c13a1b0362ea2
Jonne Williamson <jonne.williamson@psil.fi>
2026-09-08 16:08:26 +0300

    rainbow delimiters

---
+++ lua/custom/plugins/rainbow_delimiters.lua
@@ -0,0 +1,12 @@
+vim.pack.add { gh 'hiphish/rainbow-delimiters.nvim' }
+require('rainbow-delimiters.setup').setup {
+  -- strategy = {
+  --   -- ...
+  -- },
+  -- query = {
+  --   -- ...
+  -- },
+  -- highlight = {
+  --   -- ...
+  -- },
+}
